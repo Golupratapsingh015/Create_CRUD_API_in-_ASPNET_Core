2ca55ce760300cb4996b3f8bb792d235f0b11259
Vishal Kumar Singh <vishalpratapsingh143047@gmail.com>
2026-05-13 00:22:13 +0530

    add

---
+++ Create_CRUD_API_in _ASPNET_Core/Create_CRUD_API_in _ASPNET_Core/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Create_CRUD_API_in__ASPNET_Core.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
